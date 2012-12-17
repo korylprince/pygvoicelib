7c9abacb26c43390791f8fd13fab337c4270519e
Kory Prince <korylprince@gmail.com>
2012-12-17 16:28:10 -0600

    now a test client is generated.

--- get_auth.py
+++ get_auth.py
@@ -4,12 +4,13 @@ username = raw_input('username:')
 apppass = raw_input('application password:')
 client = pygvoicelib.GoogleVoice(username,apppass)
 client.validate_credentials()
-print "\nusername:"
-print username
-print "\napplication password:"
-print apppass
-print "\nauth_token:"
-print client.auth_token
-print "\nrnr_se:"
-print client.rnr_se
-print '\n'
+print """
+import pygvoicelib                                                                                                                          
+username="{0}"
+apppass="{1}"
+auth_token="{2}"
+rnr_se="{3}"
+client = pygvoicelib.GoogleVoice(username,apppass,auth_token,rnr_se)
+#replace number with phone number below
+#client.sms(number,"test")
+""".format(username,apppass,client.auth_token,client.rnr_se)
